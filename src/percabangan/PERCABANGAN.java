ffdc1ecbd7da0f3bf9f40b7b1a08489edaf17c9c
Muhammad Sonhaji Akbar <mson.akbar@gmail.com>
2020-11-11 20:12:47 +0700

    Percabangan

---
+++ src/percabangan/PERCABANGAN.java
@@ -0,0 +1,14 @@
+package percabangan;
+
+public class PERCABANGAN {
+
+    public static void main(String[] args) {
+        
+       for(int i=0; i<=3 ;i++ ){
+           
+           System.out.println("****");
+           
+       } 
+
+    }
+}
